dc8001fccc3c4d75450fee99ed4d4358c8e0af89
yongchao.zhang <yongchao.zhang@hojoai.com>
2026-08-14 20:16:01 +0800

    feat: init

--- clients/liangzui-ai-app/src-tauri/src/commands.rs
+++ clients/liangzui-ai-app/src-tauri/src/commands.rs
@@ -1,11 +1,9 @@
-mod commands;
-
 #[tauri::command]
-fn greet(name: &str) -> String {
+pub fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[tauri::command]
-fn say_hello(name: &str) -> String {{
+pub fn say_hello(name: &str) -> String {
     format!("Hello, from Rust! I'm {}", name)
 }
